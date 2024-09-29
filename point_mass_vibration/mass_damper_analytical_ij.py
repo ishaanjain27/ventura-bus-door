f34419d32c08b233be534f386f0bbe08e9c7cc27
ishaanjain27 <ijain.cal@gmail.com>
2024-09-29 18:53:28 +0200

    working mass damper analytical solution and plot

--- point_mass_vibration/mass_damper_analytical_ij.py
+++ point_mass_vibration/mass_damper_analytical_ij.py
@@ -28,11 +28,26 @@ C1, C2 = sp.symbols('C1 C2')
 solution = sp.solve([boundary_eq1, boundary_eq2], (C1, C2))
 final_solution = x_general.subs(solution)
 
+# print the analytical solution
 print(final_solution)
 
+# numerical values of constants in si units
+m_val = 1
+c_val = 0.2
+k_val = 5
+
+# substitute constants
+numerical_solution = final_solution.subs({m: m_val, c: c_val, k: k_val})
+
 # plotting
-t_domain = np.linspace(0, 10, 50)
-x_numeric = sp.lambdify(t, final_solution, 'numpy')
+t_domain = np.linspace(0, 50, 500)
+x_numeric = sp.lambdify(t, numerical_solution, 'numpy')
+x_vals = x_numeric(t_domain)
 
-
-# print the solution
+# Plot the function
+plt.plot(t_domain, x_vals)
+plt.xlabel('t')
+plt.ylabel('x(t)')
+plt.title('mass damper system with initial displacement')
+plt.grid(True)
+plt.show()
