19a839c9c82520b3dc8b1d920dd1ab0059af0acc
Simranjeet2023 <S.SINGH-11@student.tudelft.nl>
2025-01-15 13:07:54 +0100

    Spring boundaries

--- euler_beam_equation/Euler_Bernoulli_static.jl
+++ euler_beam_equation/Euler_Bernoulli_static.jl
@@ -4,6 +4,8 @@ using LinearAlgebra
 n = 100 # Number of nodes 
 L = 5  # Beam length
 EI = 70e6 * 5e-6  # Flexural rigidity
+spring_stiffness_left = 100
+spring_stiffness_right = 1000
 
 x = range(0.0, L, length=n)
 dx = L / (n-1)
@@ -21,39 +23,38 @@ function assemble_matrix(n, dx)
     return A 
 end
 
-function apply_boundary_conditions(left, right, A, rhs, n, dx)
-
-    k1 = 6
-    k2 = -4
-    k3 = 1
+# Apply boundary conditions (cl = clmaped, ss = simply supported, fr = free end, spring = with a spring at the end)
+function apply_boundary_conditions(left, right, A, load, n, dx)
+    k1, k2, k3 = 6, -4, 1
 
     if left == "cl"
-        A[1, 2] += k2
-        A[1, 3] += k3
-        A[2, 2] += k3
-        A[1, :] .= 0; A[1, 1] = 1
+        A[1, 2] += k2; A[1, 3] += k3; A[2, 2] += k3
+        A[1, :] .= 0; A[1, 1] = 1; load[1] = 0
     elseif left == "fr"
         A[1, 1] += k2 + 4k3; A[1, 2] += k2 - 4k3; A[1, 3] += -k2 + k3
         A[2, 1] += k3; A[2, 2] += k3; A[2, 3] += -k3
     elseif left == "ss"
         A[1, 2] += -k2; A[1, 3] += -k3; A[2, 2] += -k3
-        A[1, :] .= 0; A[1, 1] = 1
+        A[1, :] .= 0; A[1, 1] = 1; load[1] = 0
+    elseif left == "spring"
+        A[1, 1] += k2 + 4k3 - k2*dx^3*spring_stiffness_left/EI + 2*k3*dx^3*spring_stiffness_left/EI; A[1, 2] += k2 - 4k3; A[1, 3] += -k2 + k3
+        A[2, 1] += k3 - spring_stiffness_left*dx^3*k3/EI; A[2, 2] += k3; A[2, 3] += -k3
     end
 
     if right == "cl"
         A[end-1, end-1] += k3; A[end, end-2] += k3; A[end, end-1] += k2
-        A[end, :] .= 0; A[end, end] = 1
+        A[end, :] .= 0; A[end, end] = 1; load[end] = 0
     elseif right == "fr"
         A[end, end] += k2 + 4k3; A[end, end-1] += k2 - 4k3; A[end, end-2] += -k2 + k3
         A[end-1, end] += k3; A[end-1, end-1] += k3; A[end-1, end-2] += -k3
     elseif right == "ss"
         A[end-1, end-1] += -k3; A[end, end-2] += -k3; A[end, end-1] += -k2
-        A[end, :] .= 0; A[end, end] = 1
+        A[end, :] .= 0; A[end, end] = 1; load[end] = 0
+    elseif right == "spring"
+        A[end, end] += k2 + 4k3 - k2*dx^3*spring_stiffness_right/EI + 2*k3*dx^3*spring_stiffness_right/EI ; A[end, end-1] += k2 - 4k3; A[end, end-2] += -k2 + k3
+        A[end-1, end] += k3 - spring_stiffness_right*dx^3*k3/EI; A[end-1, end-1] += k3; A[end-1, end-2] += -k3
     end
-
-    rhs[2] = 0.0 
-    rhs[end-1] = 0.0  
-    return A, rhs
+    return A, load
 end
 
 function uniform_load(n, L, EI, x)
@@ -70,8 +71,8 @@ end
 
 A0 = assemble_matrix(n, dx)
 
-rhs = uniform_load(n, L, EI, x)
-A, rhs = apply_boundary_conditions("cl", "fr", A0, rhs, n, dx) 
+rhs = uniform_load(n, L, EI, x) #right hand side vector
+A, rhs = apply_boundary_conditions("ss", "ss", A0, rhs, n, dx) 
 A = A/dx^4
 
 ####   MODAL ANALYSIS
@@ -92,7 +93,7 @@ sorted_indices = sortperm(frequencies)
 eigvalues_sorted = eigvalues[sorted_indices]
 eigvectors_sorted = eigvectors[:, sorted_indices]
 frequencies_sorted = frequencies[sorted_indices]
-# Plot the first 6 modes
+
 plot(x, eigvectors_sorted[:, 1], label="Mode 1, Frequency=$(round(frequencies_sorted[1], digits=2))", legend=:bottomleft)
 for i in 2:6
     plot!(x, eigvectors_sorted[:, i], label="Mode $(i), Frequency=$(round(frequencies_sorted[i], digits=2))")
@@ -111,3 +112,4 @@ max_deflection_location = x[argmin(w)]
 println("Maximum Deflection: ", max_deflection, " at x = ", max_deflection_location)
 
 plot(x, w, label="Deflection w",xlabel="x (m)", ylabel="Deflection (mm)",title="Beam Deflection",legend=:top)
+
